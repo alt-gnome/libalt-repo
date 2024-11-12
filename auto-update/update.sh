872fdf16867c57b35898abbb1eba5b334db8f7b4
Vladimir Vaskov <rirusha@altlinux.org>
2024-11-12 06:59:39 +0300

    cu: update

--- auto-update/update.sh
+++ auto-update/update.sh
@@ -2,5 +2,5 @@
 # HAVE TO START FROM REPO ROOT
 
 git clone https://github.com/Rirusha/altrepo2vala.git subprojects/altrepo2vala
-python3 subprojects/altrepo2vala/generator.py 'Vladimir Vaskov' lib
-python3 auto-update/update_meson.py lib/meson.build
+python3.12 subprojects/altrepo2vala/generator.py 'Vladimir Vaskov' lib
+python3.12 auto-update/update_meson.py lib/meson.build
