9b9864765266af1a6aa7db9ba5fb538a3597a679
Vladimir Vaskov <rirusha@altlinux.org>
2024-11-13 16:14:36 +0300

    feat: improve update script

--- auto-update/update.sh
+++ auto-update/update.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 # HAVE TO START FROM REPO ROOT
 
-git clone https://github.com/Rirusha/altrepo2vala.git subprojects/altrepo2vala
-python3.12 subprojects/altrepo2vala/generator.py 'Vladimir Vaskov' lib
+rm -rf lib/client.vala lib/objects/*
+
+git clone https://github.com/Rirusha/altrepo2vala.git auto-update/altrepo2vala
+echo "*" >> auto-update/altrepo2vala/.gitignore
+
+python3.12 auto-update/altrepo2vala/generator.py 'Vladimir Vaskov' lib
 python3.12 auto-update/update_meson.py lib/meson.build
