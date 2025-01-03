098f746bc96c54107f742907590afa4f251b3fb6
Vladimir Vaskov <rirusha@altlinux.org>
2025-01-03 11:54:37 +0300

    auto-update: Fix if

--- auto-update/update.sh
+++ auto-update/update.sh
@@ -6,7 +6,7 @@ set -e
 current_api_version=$(python3 auto-update/print_api_version.py)
 tag_name="v$current_api_version"
 
-if [ $tag_name = $(git describe --tags --abbrev=0) ]; then
+if [ $tag_name == $(git describe --tags --abbrev=0) ]; then
     echo "Nothing to do"
     exit 0
 fi
