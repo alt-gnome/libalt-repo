be2ca3675783a1024cdfc8463bac1fa95ebfa2a9
Vladimir Vaskov <rirusha@altlinux.org>
2025-09-03 16:25:54 +0300

    ci: Fix update

--- auto-update/update.sh
+++ auto-update/update.sh
@@ -21,7 +21,7 @@ rm -rf lib/client.vala lib/objects/*
 git clone https://altlinux.space/rirusha/altrepo2vala.git auto-update/altrepo2vala
 echo "*" >> auto-update/altrepo2vala/.gitignore
 
-python3.12 auto-update/altrepo2vala/generator.py 'Vladimir Vaskov' lib
+python3.12 auto-update/altrepo2vala/generator.py 'Vladimir Vaskov' lib 2
 python3.12 auto-update/update_meson.py lib/meson.build
 
 git add .
